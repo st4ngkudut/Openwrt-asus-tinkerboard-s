a401bc9ddbb7d9c3972ec82417a4fb596dc0015d
st4ngkudut <94985658+st4ngkudut@users.noreply.github.com>
2025-07-29 00:41:00 +0700

    Update makefile-snippet.mk

--- openwrt-device/makefile-snippet.mk
+++ openwrt-device/makefile-snippet.mk
@@ -1,7 +1,7 @@
 
 define Device/asus_tinker_board_s
   DEVICE_TITLE := Asus Tinker Board S
-  DEVICE_DTS := rk3288-tinker-s
-  DEVICE_PACKAGES := kmod-r8169 kmod-usb-core kmod-usb2 kmod-usb3 kmod-fs-ext4 block-mount base-files blkid ca-bundle dnsmasq dropbear e2fsprogs firewall4 fstools kmod-fs-vfat kmod-nft-offload libc libgcc libustream-mbedtls logd mkf2fs mtd netifd nftables odhcp6c odhcpd-ipv6only opkg ppp ppp-mod-pppoe procd-ujail uci uclient-fetch urandom-seed urngd kmod-amazon-ena kmod-e1000e kmod-vmxnet3 kmod-rtc-rx8025 kmod-i2c-mux-pca954x kmod-gpio-pca953x partx-utils kmod-wdt-sp805 kmod-mvneta kmod-mvpp2 kmod-fsl-dpaa1-net kmod-fsl-dpaa2-net kmod-fsl-enetc-net kmod-dwmac-imx kmod-fsl-fec kmod-dwmac-rockchip kmod-dwmac-sun8i kmod-phy-aquantia kmod-phy-broadcom kmod-phy-marvell kmod-phy-marvell-10g kmod-sfp kmod-atlantic kmod-bcmgenet kmod-octeontx2-net kmod-renesas-net-avb kmod-phy-realtek kmod-phy-smsc luci
+  DEVICE_DTS := rk3288-tinker
+  DEVICE_PACKAGES := kmod-r8169 kmod-usb-core kmod-usb2 kmod-usb3 kmod-fs-ext4 block-mount
 endef
 TARGET_DEVICES += asus_tinker_board_s
